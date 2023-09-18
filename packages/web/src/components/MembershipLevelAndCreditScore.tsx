e1a9df7bdb5d138608680cf9de8cad5e37f31261
0xZensh <txr1883@gmail.com>
2023-09-18 08:10:42 +0800

    feat: update documents for Yiwen Coin and Credits

--- packages/web/src/components/MembershipLevelAndCreditScore.tsx
+++ packages/web/src/components/MembershipLevelAndCreditScore.tsx
@@ -16,20 +16,15 @@ export default function MembershipLevelAndCreditScore(
       <p>
         {intl.formatMessage({
           defaultMessage:
-            '信用分和会员等级是 Yiwen AI 平台为鼓励用户创造价值而设立的一种激励机制，用户激活会员后，在平台消费或者创作内容获得收益时，会获得亿文币对应数量的信用分。信用分只会增加不会消耗减少，只用于计算用户的会员等级。用户会员等级越高，在平台的荣誉和权益就越高，包括修改用户名、提前体验新功能、降低创作者收益服务费比例等，具体荣誉和权益会随着平台发展而不断增加。',
+            '信用分和会员等级是平台为鼓励用户创造价值而设立的一种激励机制，用户激活会员后，在平台消费或者创作内容获得收益时，会获得亿文币对应数量的信用分。信用分越多，会员等级越高。',
         })}
       </p>
-      <h3>
-        {intl.formatMessage({
-          defaultMessage: '详细规则',
-        })}
-      </h3>
       <ol>
         <li>
           <p>
             {intl.formatMessage({
               defaultMessage:
-                '用户完成注册时，还不是会员，会员等级显示为 LV0。平台系统赠送的亿文币只可用于一般消费，如支付翻译功能费用，不能用于打赏等创作收益类消费，消费也不会获得信用分；',
+                '非会员：用户完成注册时，还不是会员，会员等级显示为 LV0。用户钱包中的亿文币只能用于一般消费，如支付翻译功能费用，不能用于打赏等创作收益类消费，消费时或获得收益时都不会获得信用分。',
             })}
           </p>
         </li>
@@ -37,7 +32,7 @@ export default function MembershipLevelAndCreditScore(
           <p>
             {intl.formatMessage({
               defaultMessage:
-                '用户设置好敏感操作口令（又称 PIN 码），并完成首次 HK$10 以上的充值时，才激活会员，此时初始信用分为 10，会员等级显示为 LV1，平台系统赠送的亿文币可用于所有消费；',
+                '激活会员：用户完成首次 50 文以上的充值时，才激活会员，初始信用分为 10，会员等级显示为 LV1，用户钱包中的亿文币能用于所有类型的消费，消费时或获得收益时会获得等额信用分。',
             })}
           </p>
         </li>
@@ -45,15 +40,30 @@ export default function MembershipLevelAndCreditScore(
           <p>
             {intl.formatMessage({
               defaultMessage:
-                '用户激活会员后，在平台消费或者创作内容获得收益时，会获得亿文币对应数量的信用分，具体规则如下：',
+                '系统奖励：对平台做出了特殊贡献的用户，系统会奖励信用分。比如，用户发现了平台重大漏洞并及时报告，系统奖励 1000 ～ 1,000,000 个信用分。',
+            })}
+          </p>
+        </li>
+        <li>
+          <p>
+            {intl.formatMessage({
+              defaultMessage:
+                '会员等级：会员等级所要求的信用分计算公式为：信用分 = 10 ** 会员等级，LV2 需要 100 分，LV3 需要 1000 分，LV4 需要 10,000 分。',
+            })}
+          </p>
+        </li>
+        <li>
+          <p>
+            {intl.formatMessage({
+              defaultMessage:
+                '会员权益：目前不同会员等级的权益设计如下，更多荣誉和权益会随着平台发展而不断增加：',
             })}
           </p>
           <ol>
             <li>
               <p>
                 {intl.formatMessage({
-                  defaultMessage:
-                    '用户在平台消费时，获得消费亿文币对应数量的信用分。比如消费了 10 亿文币，即可获得 10 个信用分；',
+                  defaultMessage: 'LV2 以上会员可以使用 GPT-4 模型进行翻译；',
                 })}
               </p>
             </li>
@@ -61,48 +71,7 @@ export default function MembershipLevelAndCreditScore(
               <p>
                 {intl.formatMessage({
                   defaultMessage:
-                    '用户创作内容获得收益时，获得收益的亿文币对应数量的信用分。比如读者打赏了 100 亿文币，该读者即可获得 100 个信用分；系统扣除服务费后，创作者获得 70% 的收益，即 70 亿文币，同时也获得 70 个信用分；',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '对平台做出了特殊贡献的用户，平台会赠送信用分。比如，用户在平台发现了重大漏洞并及时向平台报告，平台会赠送 1000 ～ 1,000,000 个信用分；',
-                })}
-              </p>
-            </li>
-          </ol>
-        </li>
-        <li>
-          <p>
-            {intl.formatMessage({
-              defaultMessage:
-                '不同会员等级所要求的信用分计算公式为：信用分 = (10 ** (会员等级 - 1)) * 10，所以 LV2 需要 100 分，LV3 需要 1000 分，LV4 需要 10,000 分；',
-            })}
-          </p>
-        </li>
-        <li>
-          <p>
-            {intl.formatMessage({
-              defaultMessage:
-                '目前不同会员等级的权益设计如下，更多荣誉和权益会随着平台发展而不断增加：',
-            })}
-          </p>
-          <ol>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage: 'LV2 以上会员可以使用 GPT4 模型进行翻译；',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    'LV3 以上会员可以修改用户名，可以免费修改一次6位及以上的用户名，修改更多次用户名或使用更短用户名需要支付亿文币；可以创建群组；可以调用开放 API；',
+                    'LV3 以上会员可以修改用户名、创建群组、调用开放 API等（相关功能待上线）；',
                 })}
               </p>
             </li>
@@ -137,7 +106,21 @@ export default function MembershipLevelAndCreditScore(
             <li>
               <p>
                 {intl.formatMessage({
-                  defaultMessage: 'LV8 以上会员的创作者收益服务费为 15%。',
+                  defaultMessage: 'LV8 以上会员的创作者收益服务费为 15%；',
+                })}
+              </p>
+            </li>
+            <li>
+              <p>
+                {intl.formatMessage({
+                  defaultMessage: 'LV9 以上会员的创作者收益服务费为 12%；',
+                })}
+              </p>
+            </li>
+            <li>
+              <p>
+                {intl.formatMessage({
+                  defaultMessage: 'LV10 以上会员的创作者收益服务费为 9%。',
                 })}
               </p>
             </li>
--- packages/web/src/components/YiwenCoin.tsx
+++ packages/web/src/components/YiwenCoin.tsx
@@ -9,116 +9,14 @@ export default function YiwenCoin(props: React.HTMLAttributes<HTMLElement>) {
       <p>
         {intl.formatMessage({
           defaultMessage:
-            '亿文币是 Yiwen AI 平台的官方虚拟货币，用于方便用户完成平台内的支付活动。用户可以用亿文币支付平台上的服务、作品费用，或给作品打赏等。亿文币不能转账，不能提现，只能用于消费。创作者通过平台创作获得的亿文币收益才能转换成亿文元，进而提现。',
-        })}
-      </p>
-      <h3>
-        {intl.formatMessage({
-          defaultMessage: '详细规则',
-        })}
-      </h3>
-      <ol>
-        <li>
-          <p>
-            {intl.formatMessage({
-              defaultMessage:
-                '注册礼：用户注册时，平台系统赠送 100 文，只能用于消费；',
-            })}
-          </p>
-        </li>
-        <li>
-          <p>
-            {intl.formatMessage({
-              defaultMessage:
-                '邀请好友奖励：用户邀请好友注册并激活会员时，平台系统按每个好友赠送 10 文，用户每周奖励上限为 10,000 文（该活动奖励方案会定期调整），只能用于消费；',
-            })}
-          </p>
-        </li>
-        <li>
-          <p>
-            {intl.formatMessage({
-              defaultMessage: '平台其它活动奖励，只能用于消费；',
-            })}
-          </p>
-        </li>
-        <li>
-          <p>
-            {intl.formatMessage({
-              defaultMessage:
-                '用户通过充值渠道充值，兑换比例为 HK$1 = WEN10，只能用于消费。注意：',
-            })}
-          </p>
-          <ol>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '充值支付渠道可能会收取一定的手续费，实际到账金额会扣除该手续费，具体手续费由支付渠道决定；',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '充值最低额度为 HK$10，最高额度为 HK$10,000，超过最高额度的充值，平台会拒绝接受；',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '根据相关法律，亿文币属于数字化商品，不适用于无理由退款，用户充值后，不能退款。如果因为其它不得已的原因需要退款，用户需要完成实名认证并提供相关证据截图，平台会按照实际情况进行退款，并且会收取 5% 的手续费，最低手续费为 10 文；',
-                })}
-              </p>
-            </li>
-          </ol>
-        </li>
-        <li>
-          <p>
-            {intl.formatMessage({
-              defaultMessage:
-                '用户在平台创作内容获得亿文币收益，包括付费阅读、读者打赏等，可用于消费和转换成亿文元。注意：',
-            })}
-          </p>
-          <ol>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '读者付费阅读、打赏作品等支付的亿文币，平台系统将扣除 15% ～ 30% 的服务费，最低服务费为 1 文，创作者获得的收益为 70% ～ 85%；',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '系统实际扣除的服务费比例与用户会员等级相关，用户会员等级越高，扣除的服务费比例越低。',
-                })}
-              </p>
-            </li>
-          </ol>
-        </li>
-      </ol>
-      <h3>
-        {intl.formatMessage({
-          defaultMessage: '转换为亿文元进行提现',
-        })}
-      </h3>
-      <p>
-        {intl.formatMessage({
-          defaultMessage:
-            '亿文元是 Yiwen AI 平台的官方虚拟物品，用于方便用户将平台创作收益获得的亿文币转换为法币。亿文元不能转账，不能消费，只能用于提现或转换成亿文币消费。',
+            '亿文币是平台的官方虚拟货币，单位为“文”，代号为 WEN，用于方便用户完成平台内的支付活动。',
         })}
       </p>
       <ol>
         <li>
           <p>
             {intl.formatMessage({
-              defaultMessage:
-                '用户将平台收益获得（非充值和系统赠送）的亿文币转换成亿文元，兑换比例为 WEN10 = YW$1，最低转换值为 10 文。平台会收取 0.1% 的转换手续费，最低手续费为 1 文；',
+              defaultMessage: '价值：亿文币价值锚定港币，1 HK$ = 10 WEN。',
             })}
           </p>
         </li>
@@ -126,7 +24,7 @@ export default function YiwenCoin(props: React.HTMLAttributes<HTMLElement>) {
           <p>
             {intl.formatMessage({
               defaultMessage:
-                '用户可以将亿文元换成亿文币，兑换比例为 YW$1 = WEN10，平台不会收取转换手续费；',
+                '充值：用户通过充值渠道充值，充值最低额度为 50 文，最高额度为 1,000,000 文。充值的亿文币只能用于消费。',
             })}
           </p>
         </li>
@@ -134,51 +32,33 @@ export default function YiwenCoin(props: React.HTMLAttributes<HTMLElement>) {
           <p>
             {intl.formatMessage({
               defaultMessage:
-                '用户可以将亿文元提现为法币，默认提现为港币，兑换比例为 YW$1 = HK$1，未来可支持提现为其它法币，兑换比例按照港币与其它法币的汇率计算。注意：',
+                '奖励：用户注册时，系统赠送 100 文；用户邀请的好友注册并激活会员时，系统按每个好友赠送 50 文。奖励的亿文币只能用于消费。',
             })}
           </p>
-          <ol>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '用户需要完成实名认证，并绑定与实名一致的提现渠道（逐步支持支付宝、银行卡、对公账号等）；',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '提现时，平台不会收取手续费，但提现渠道可能会收取一定的手续费，具体手续费由提现渠道决定；',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '根据税务合规要求，平台可能会代缴代扣税费，具体税费由提现渠道收款账号所在地的税务要求决定；',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '用户提现的实际到账金额为提现金额减去提现渠道收取的手续费和平台代缴代扣的税费。',
-                })}
-              </p>
-            </li>
-            <li>
-              <p>
-                {intl.formatMessage({
-                  defaultMessage:
-                    '亿文元渠道暂未开通，如需提现可联系我们进行提现（邮件）',
-                })}
-              </p>
-            </li>
-          </ol>
+        </li>
+        <li>
+          <p>
+            {intl.formatMessage({
+              defaultMessage:
+                '消费：消费活动包括支付翻译功能费用、付费阅读、打赏作品、订阅群组、红包等（部分功能待上线）。',
+            })}
+          </p>
+        </li>
+        <li>
+          <p>
+            {intl.formatMessage({
+              defaultMessage:
+                '提现：读者付费阅读、打赏作品、订阅群组等支付亿文币给创作者，系统扣除服务费后，创作者获得的收益可以提现（提现功能待上线）。',
+            })}
+          </p>
+        </li>
+        <li>
+          <p>
+            {intl.formatMessage({
+              defaultMessage:
+                '服务费：系统扣除服务费的比例为 30% ～ 9%，最低为 1 文，用户会员等级越高，扣除的服务费比例越低。',
+            })}
+          </p>
         </li>
       </ol>
     </PlainArticle>
