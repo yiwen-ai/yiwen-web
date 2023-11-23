ee1a6e663788315dcdd0f2de9e809ec331f70e04
0xZensh <txr1883@gmail.com>
2023-11-23 21:52:35 +0800

    fix: fix vite-pwa option

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -3,3 +3,12 @@ import App from './App'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(<App />)
+
+if ('serviceWorker' in navigator) {
+  const scope = 'https://' + document.location.host
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(scope + '/sw.js', {
+      scope,
+    })
+  })
+}
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,6 +5,14 @@ import { defineConfig } from 'vite'
 import { checker } from 'vite-plugin-checker'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const scope = process.env['npm_package_scripts_build']?.includes('testing')
+  ? 'https://www.yiwen.ltd'
+  : 'https://www.yiwen.ai'
+const cdnPrefix =
+  scope === 'https://www.yiwen.ltd'
+    ? 'https://cdn.yiwen.pub/dev/web/'
+    : 'https://cdn.yiwen.pub/web/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,15 +21,25 @@ export default defineConfig({
     checker({ typescript: true }),
     svgr({ ref: true, titleProp: true }),
     VitePWA({
+      scope,
+      buildBase: cdnPrefix,
+      injectRegister: null,
       registerType: 'autoUpdate',
       workbox: {
         clientsClaim: true,
         skipWaiting: true,
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+        globPatterns: [
+          '**/*.{js,css,html,txt,webmanifest,svg,png,jpg,jpeg,ico,ttf,woff2,woff}',
+        ],
+        modifyURLPrefix: {
+          '': cdnPrefix,
+        },
       },
       devOptions: {
         enabled: true,
       },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'logo.svg'],
+      includeManifestIcons: true,
       manifest: {
         'name': 'Yiwen',
         'short_name': 'Yiwen',
@@ -32,23 +50,23 @@ export default defineConfig({
         'description': 'AI-based Translingual Knowledge Content Platform.',
         'icons': [
           {
-            'src': 'favicon-64x64.png',
+            'src': cdnPrefix + 'favicon-64x64.png',
             'sizes': '64x64',
             'type': 'image/png',
           },
           {
-            'src': 'favicon-192x192.png',
+            'src': cdnPrefix + 'favicon-192x192.png',
             'sizes': '192x192',
             'type': 'image/png',
           },
           {
-            'src': 'favicon-512x512.png',
+            'src': cdnPrefix + 'favicon-512x512.png',
             'sizes': '512x512',
             'type': 'image/png',
             'purpose': 'any',
           },
           {
-            'src': 'favicon-512x512-maskable.png',
+            'src': cdnPrefix + 'favicon-512x512-maskable.png',
             'sizes': '512x512',
             'type': 'image/png',
             'purpose': 'maskable',
